958727378602d17cee8b46aff35f8326ac4b9f62
DevDanny <albuquerqud@gmailcom>
2022-05-12 22:42:20 +0200

    refactor error display

--- TradeQuarter/Views/TickersView.swift
+++ TradeQuarter/Views/TickersView.swift
@@ -17,7 +17,7 @@ struct TickersView: View {
     
     // MARK: - Search
     @State private var searchQuery: String = ""
-    
+
     
     var body: some View {
         NavigationView {
@@ -39,13 +39,10 @@ struct TickersView: View {
                         }
                 }
             }else{
-                VStack{
-                    Text((searchQuery.isEmpty ? "Marketplace" : "Search") + " Unavailable").font(.headline)
-                    Text(!viewModel.isConnected ? "TradeQuarter isn't connected to the internet." : "TradeQuarter has no data to display").font(.callout)
-                    if let error = viewModel.error {
-                        Text(error.localizedDescription).font(.callout)
-                    }
-                }.foregroundColor(.gray)
+                VStack(spacing: 8){
+                    Text(errorTitle).font(.headline)
+                    Text(errorSubtitle).font(.callout).multilineTextAlignment(.center)
+                }.foregroundColor(.gray).padding(32)
             }
         }
         .navigationViewStyle(StackNavigationViewStyle())
@@ -59,6 +56,51 @@ struct TickersView: View {
         }
         .onAppear(){
             viewModel.syncTickersEvery5s()
+        }
+    }
+    
+    //MARK: - Dynamic labels
+    private var errorTitle: String {
+        if(searchQuery.isEmpty){
+            if(viewModel.isConnected){
+                if let error = viewModel.error {
+                    switch(error){
+                    case .networkError(error: _):
+                        return "Network Error"
+                    case .peristenceError(error: _):
+                        return "Marketplace Unavailable"
+                    case .unknownError(error: _):
+                        return "Marketplace Unavailable"
+                    }
+                }
+            }else{
+                return "Not connected"
+            }
+            return "Marketplace Unavailable"
+        }else{
+            return ("No results found")
+        }
+    }
+    
+    private var errorSubtitle: String {
+        if(searchQuery.isEmpty){
+            if(viewModel.isConnected){
+                if let error = viewModel.error {
+                    switch(error){
+                    case .networkError(error: _):
+                        return "TradeQuarter can't fetch data, try again later."
+                    case .peristenceError(error: let error):
+                        return error.localizedDescription
+                    case .unknownError(error: let error):
+                        return error.localizedDescription
+                    }
+                }
+            }else{
+                return "TradeQuarter isn't connected to the internet. Check your connection and try again."
+            }
+            return "TradeQuarter can't fetch data, try again later."
+        }else{
+            return ("Try modifying your search")
         }
     }
 }
